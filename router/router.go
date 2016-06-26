99742401be23202413e54499fb4238b6fddfb93c
Brandon Okert <brandon.okert@hootsuite.com>
2016-06-26 11:57:27 -0700

    Added request timing to /noise endpoint

--- router/router.go
+++ router/router.go
@@ -5,14 +5,15 @@ import (
 
 	"github.com/bcokert/terragen/controller"
 	"github.com/bcokert/terragen/endpoint"
+	"github.com/bcokert/terragen/middleware"
 )
 
 // CreateDefaultRouter returns a router with all the default routes configured
 func CreateDefaultRouter(server *controller.Server) http.Handler {
 	router := http.NewServeMux()
-	router.HandleFunc("/amiup", endpoint.Amiup)
+	router.HandleFunc("/amiup", middleware.TimedRequest(endpoint.Amiup, "Amiup"))
 
-	router.HandleFunc("/noise", server.Noise)
+	router.HandleFunc("/noise", middleware.TimedRequest(server.Noise, "Noise"))
 
 	return router
 }
