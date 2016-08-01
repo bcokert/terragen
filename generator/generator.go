9139eabb5e36835cb128384229355e0c2e22ca8a
Brandon Okert <brandon.okert@hootsuite.com>
2016-08-01 15:14:36 -0700

    Added a perlin noise implementation

--- generator/generator.go
+++ generator/generator.go
@@ -1,18 +1,42 @@
 package generator
 
 import (
-	"math/rand"
-
+	"github.com/bcokert/terragen/interpolation"
 	"github.com/bcokert/terragen/noise"
+	"github.com/bcokert/terragen/random"
+	"github.com/bcokert/terragen/vector"
 )
 
-// A Generator creates a specifically seeded noise function
-type Generator func(seed int64) noise.Function
-
 // Random Builds a noise function that returns random floats in the range [0, 1]
-func Random(seed int64) noise.Function {
-	random := rand.New(rand.NewSource(seed))
+func Random(random random.Source) noise.Function {
 	return func(t []float64) float64 {
 		return random.Float64()
+	}
+}
+
+// Perlin builds a noise function that returns Lattice Gradient noise values as described by Ken Perlin
+func Perlin(cache vector.GridCache, interpolator interpolation.Interpolator) noise.Function {
+	return func(t []float64) float64 {
+		// Find the surrounding grid points of the lattice
+		topLeftX, topLeftY := int(t[0]), int(t[1])
+		gridPoints := [4][2]int{
+			[2]int{topLeftX, topLeftY},
+			[2]int{topLeftX + 1, topLeftY},
+			[2]int{topLeftX, topLeftY - 1},
+			[2]int{topLeftX + 1, topLeftY - 1},
+		}
+
+		// Generate influences by computing the dot product of a random vector and a direction vector for each grid point
+		influences := [4]float64{}
+		for i, gridPoint := range gridPoints {
+			directionVector := vector.NewVec2(t[0]-float64(gridPoint[0]), t[1]-float64(gridPoint[1]))
+			gridVector := cache.Get(gridPoint[0], gridPoint[1])
+			influences[i] = gridVector.Dot(directionVector)
+		}
+
+		// Interpolate the influence values to get the noise value
+		avgAB := interpolator(t[0]-float64(topLeftX), influences[0], influences[1])
+		avgCD := interpolator(t[0]-float64(topLeftX), influences[2], influences[3])
+		return interpolator(t[1]-float64(topLeftY-1), avgCD, avgAB)
 	}
 }
--- generator/generator_test.go
+++ generator/generator_test.go
@@ -1,23 +1,25 @@
 package generator_test
 
 import (
-	"math/rand"
 	"testing"
 
 	"github.com/bcokert/terragen/generator"
+	"github.com/bcokert/terragen/interpolation"
 	"github.com/bcokert/terragen/noise"
+	"github.com/bcokert/terragen/random"
+	"github.com/bcokert/terragen/vector"
 )
 
 func TestRandom(t *testing.T) {
 	testCases := map[string]struct {
-		Seed              int64
+		Source            random.Source
 		ExpectedFnCreator func() noise.Function
 		ExpectedDimension int
 	}{
 		"basic 1d": {
-			Seed: 42,
+			Source: random.NewDefaultSource(42),
 			ExpectedFnCreator: func() noise.Function {
-				rand := rand.New(rand.NewSource(42)).Float64
+				rand := random.NewDefaultSource(42).Float64
 				return func(t []float64) float64 {
 					return rand()
 				}
@@ -25,9 +27,9 @@ func TestRandom(t *testing.T) {
 			ExpectedDimension: 1,
 		},
 		"basic 2d": {
-			Seed: 99,
+			Source: random.NewDefaultSource(99),
 			ExpectedFnCreator: func() noise.Function {
-				rand := rand.New(rand.NewSource(99)).Float64
+				rand := random.NewDefaultSource(99).Float64
 				return func(t []float64) float64 {
 					return rand()
 				}
@@ -37,9 +39,52 @@ func TestRandom(t *testing.T) {
 	}
 
 	for name, testCase := range testCases {
-		noiseFunction := generator.Random(testCase.Seed)
+		noiseFunction := generator.Random(testCase.Source)
 		if !noiseFunction.IsEqual(testCase.ExpectedFnCreator(), testCase.ExpectedDimension) {
 			t.Errorf("%s failed. Noise function did not equal expected function", name)
+		}
+	}
+}
+
+func TestPerlin(t *testing.T) {
+	testCases := map[string]struct {
+		Cache        vector.GridCache
+		Interpolator interpolation.Interpolator
+	}{
+		"grid edges": {
+			Cache:        &vector.MockGridCache{},
+			Interpolator: interpolation.NewInterpolator(interpolation.LinearEase),
+		},
+	}
+
+	for name, testCase := range testCases {
+
+		testNoiseFunction := generator.Perlin(testCase.Cache, testCase.Interpolator)
+		expectedNoiseFunction := func(input []float64) float64 {
+			a, b := input[0], input[1]
+
+			topLeft := [2]int{int(a), int(b)}
+			topRight := [2]int{int(a) + 1, int(b)}
+			botLeft := [2]int{int(a), int(b) - 1}
+			botRight := [2]int{int(a) + 1, int(b) - 1}
+
+			tl := vector.NewVec2(a-float64(topLeft[0]), b-float64(topLeft[1]))
+			tr := vector.NewVec2(a-float64(topRight[0]), b-float64(topRight[1]))
+			bl := vector.NewVec2(a-float64(botLeft[0]), b-float64(botLeft[1]))
+			br := vector.NewVec2(a-float64(botRight[0]), b-float64(botRight[1]))
+
+			itl := tl.Dot(testCase.Cache.Get(topLeft[0], topLeft[1]))
+			itr := tr.Dot(testCase.Cache.Get(topRight[0], topRight[1]))
+			ibl := bl.Dot(testCase.Cache.Get(botLeft[0], botLeft[1]))
+			ibr := br.Dot(testCase.Cache.Get(botRight[0], botRight[1]))
+
+			avgAB := testCase.Interpolator(a-float64(topLeft[0]), itl, itr)
+			avgCD := testCase.Interpolator(a-float64(topLeft[0]), ibl, ibr)
+			return testCase.Interpolator(b-float64(topLeft[1]-1), avgCD, avgAB)
+		}
+
+		if !testNoiseFunction.IsEqual(expectedNoiseFunction, 2) {
+			t.Errorf("'%s' failed.", name)
 		}
 	}
 }
