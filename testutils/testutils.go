5272e6ab84060f7c27fe4a0260faa4559f12642d
Brandon Okert <brandon.okert@hootsuite.com>
2016-08-01 10:55:11 -0700

    Added an IsFloatEqual test util

--- testutils/testutils.go
+++ testutils/testutils.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"math"
 )
 
 // ExecuteTestRequest runs a request using the given router, and returns the response recorder
@@ -14,4 +15,9 @@ func ExecuteTestRequest(router http.Handler, method, url string, body io.Reader)
 	router.ServeHTTP(response, request)
 
 	return response
+}
+
+// IsFloatEqual compares two floats to see if they're close enough to be considered equal
+func IsFloatEqual(a, b float64) bool {
+	return math.Abs(a - b) <= 0.00000000000001
 }
--- testutils/testutils_test.go
+++ testutils/testutils_test.go
@@ -46,3 +46,33 @@ func TestExecuteTestRequest(t *testing.T) {
 		}
 	}
 }
+
+func TestIsFloatEqual(t *testing.T) {
+	testCases := map[string]struct {
+		A, B float64
+		Expected bool
+	}{
+		"zero": {
+			A: 0,
+			B: 0,
+			Expected: true,
+		},
+		"large": {
+			A: 623462362.23452345,
+			B: 623462362.23452345,
+			Expected: true,
+		},
+		"almost": {
+			A: 623462362.2345234,
+			B: 623462362.2345235,
+			Expected: false,
+		},
+	}
+
+	for name, testCase := range testCases {
+		result := testutils.IsFloatEqual(testCase.A, testCase.B)
+		if result != testCase.Expected {
+			t.Errorf("'%s' failed. Expected %v, received %v", name, testCase.Expected, result)
+		}
+	}
+}
